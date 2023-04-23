da8d826b1252d8228252c06ad09c2bec4d3f4e89
Sousa-Edson <85588242+Sousa-Edson@users.noreply.github.com>
2023-04-23 10:29:26 -0300

    salvando e atualizando no banco de dados

--- app/src/main/java/com/edson/androidappfirebase/view/telaprincipal/TelaPrincipal.java
+++ app/src/main/java/com/edson/androidappfirebase/view/telaprincipal/TelaPrincipal.java
@@ -1,31 +1,65 @@
 package com.edson.androidappfirebase.view.telaprincipal;
 
-import com.edson.androidappfirebase.R;
-import com.edson.androidappfirebase.databinding.ActivityTelaPrincipalBinding;
-import com.edson.androidappfirebase.view.formCadastro.FormCadastro;
-import com.edson.androidappfirebase.view.formLogin.FormLogin;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.ktx.Firebase;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
+import com.edson.androidappfirebase.databinding.ActivityTelaPrincipalBinding;
+import com.edson.androidappfirebase.view.formLogin.FormLogin;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TelaPrincipal extends AppCompatActivity {
-private ActivityTelaPrincipalBinding binding ;
+    private ActivityTelaPrincipalBinding binding;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityTelaPrincipalBinding.inflate(getLayoutInflater());
+        binding = ActivityTelaPrincipalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnDeslogar.setOnClickListener(view->{
+        binding.btnDeslogar.setOnClickListener(view -> {
             FirebaseAuth.getInstance().signOut();
             Intent intent = new Intent(this, FormLogin.class);
             startActivity(intent);
             finish();
+        });
+
+        binding.btnGravarDadosDB.setOnClickListener(view -> {
+            Map<String, Object> usuarioMap = new HashMap<>();
+            usuarioMap.put("nome", "Edson");
+            usuarioMap.put("sobrenome", "Sousa");
+            usuarioMap.put("idade", 37);
+
+            db.collection("Usuarios").document("Edson")
+                    .set(usuarioMap).addOnCompleteListener(task -> {
+                        Log.d("db", "Sucesso ao salvar dados de usuario");
+                    }).addOnFailureListener(exception -> {
+                    });
+        });
+
+        binding.btnLerDadosDB.setOnClickListener(view -> {
+            db.collection("Usuarios").document("Maria").addSnapshotListener((documento, error) -> {
+                if (documento != null) {
+                    Long idade = documento.getLong("idade");
+                    binding.txtResultadoNome.setText(documento.getString("nome"));
+                    binding.txtResultadoSobrenome.setText(documento.getString("sobrenome"));
+                    binding.txtResultadoIdade.setText(idade.toString());
+                }
+            });
+        });
+        binding.btnAtualizarDadosDB.setOnClickListener(view -> {
+            db.collection("Usuarios").document("Maria").update("sobrenome", "mudado").addOnCompleteListener(task -> {
+                Log.d("db", "Sucesso ao atualizar dados de usuario");
+            });
+
         });
     }
 }
